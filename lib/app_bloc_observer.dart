963431c8aa9c8cc3721cdcc52b082baf09178cab
Kelv1nG <kelvinmaglipon@gmail.com>
2024-12-10 00:07:47 +0800

    Update app_bloc_observer.dart
    just print changes in console

--- lib/app_bloc_observer.dart
+++ lib/app_bloc_observer.dart
@@ -7,12 +7,14 @@ class AppBlocObserver extends BlocObserver {
   @override
   void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
     super.onChange(bloc, change);
-    log('onChange(${bloc.runtimeType}, $change)');
+    // ignore: avoid_print
+    print('onChange(${bloc.runtimeType}, $change)');
   }
 
   @override
   void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
-    log('onError(${bloc.runtimeType}, $error, $stackTrace)');
+    // ignore: avoid_print
+    print('onError(${bloc.runtimeType}, $error, $stackTrace)');
     super.onError(bloc, error, stackTrace);
   }
 }
