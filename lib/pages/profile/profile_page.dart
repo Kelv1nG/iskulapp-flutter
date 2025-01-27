78a8cc966079a65ecb84016078fd83d994f97279
georgesycabaccang <georgesycabaccang@gmail.com>
2025-01-27 14:39:14 +0800

    Add conditional rendering using _isLoading to avoid some bugs and erros when no data is available yet.

--- lib/pages/profile/profile_page.dart
+++ lib/pages/profile/profile_page.dart
@@ -6,6 +6,7 @@ import 'package:school_erp/features/auth/utils.dart';
 import 'package:school_erp/models/academic_year.dart';
 import 'package:school_erp/models/guardian.dart';
 import 'package:school_erp/models/student.dart';
+import 'package:school_erp/pages/common_widgets/animation_widgets/loading_overlay.dart';
 import 'package:school_erp/pages/common_widgets/default_layout.dart';
 import 'package:school_erp/pages/profile/helpers/classes/profile_item_data.dart';
 import 'package:school_erp/pages/profile/widgets/profile_details_list.dart';
@@ -22,17 +23,18 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-    late final AuthenticatedUser user;
-    late final List<Guardian> guardians;
-    late final AcademicYear academicYear;
-    late final Student student;
+    AuthenticatedUser? user;
+    List<Guardian>? guardians;
+    AcademicYear? academicYear;
+    Student? student;
+    bool _isLoading = true;
 
     @override
     void initState() {
         super.initState();
 
         _getUserDetails();
-        if (user.role == UserRole.student) _getStudentProfileDetails();
+        if (user?.role == UserRole.student) _getStudentProfileDetails();
     }
 
     void _getUserDetails() {
@@ -41,54 +43,73 @@ class _ProfilePageState extends State<ProfilePage> {
     }
 
     void _getStudentProfileDetails() async {
-        StudentRepository studentRepository = StudentRepository();
-        GuardianRepository guardianRepository = GuardianRepository();
-        List<Guardian> guardiansOfStudent = await guardianRepository.getGuardiansOfStudent(userId: user.id);
-        Student studentDetails = await studentRepository.getStudent(userId: user.id, academicYearId: user.academicYearId);
+        try {
+            setState(() => _isLoading = true);
 
-        setState(() {
-                guardians = guardiansOfStudent;
-                student = studentDetails;
-            });
+            StudentRepository studentRepository = StudentRepository();
+            GuardianRepository guardianRepository = GuardianRepository();
+            List<Guardian> guardiansOfStudent = await guardianRepository.getGuardiansOfStudent(userId: user!.id);
+            Student studentDetails = await studentRepository.getStudent(userId: user!.id, academicYearId: user!.academicYearId);
+
+            setState(() {
+                    guardians = guardiansOfStudent;
+                    student = studentDetails;
+                });
+        } catch (e) {
+            print(e);
+        } finally {
+            setState(() => _isLoading = false);
+        }
     }
 
     @override
     Widget build(BuildContext context) {
-        final List<ProfileItemData> profileItemDataList = [
-            ProfileItemData('Student No.', student.studentNo),
-            ProfileItemData('Academic Year', '${student.academicYearStart} - ${student.academicYearEnd}'),
-            ProfileItemData('Grade Level', student.gradeLevelName!.capitalize(), CupertinoIcons.lock_fill),
-            ProfileItemData('Section', student.sectionName!.capitalize(), CupertinoIcons.lock_fill),
-            ProfileItemData('Date of Birth', DateFormat('dd MMM, yyyy').format(student.birthDate!), CupertinoIcons.lock_fill),
-            ProfileItemData('Permanent Address', 'Karol Bagh, Delhi', CupertinoIcons.lock_fill, 370),
-            ProfileItemData('Guardian Email', 'parentboth84@gmail.com', CupertinoIcons.lock_fill, 370),
-        ];
+        final List<ProfileItemData> profileItemDataList = 
+            // Show a blank list if _isLoading to avoid bugs, and errors caused by displaying
+            // data without actual data. 
+            // Can also instead display somehting else for each field (like Loading...) if deisred instead of this apporach
+            _isLoading ? []
+                : [
+                    ProfileItemData('Student No.', student!.studentNo),
+                    ProfileItemData('Academic Year', '${student!.academicYearStart} - ${student!.academicYearEnd}'),
+                    ProfileItemData('Grade Level', student!.gradeLevelName!.capitalize(), CupertinoIcons.lock_fill),
+                    ProfileItemData('Section', student!.sectionName!.capitalize(), CupertinoIcons.lock_fill),
+                    ProfileItemData('Date of Birth', DateFormat('dd MMM, yyyy').format(student!.birthDate!), CupertinoIcons.lock_fill),
+                    ProfileItemData('Permanent Address', 'Karol Bagh, Delhi', CupertinoIcons.lock_fill, 370),
+                    ProfileItemData('Guardian Email', 'parentboth84@gmail.com', CupertinoIcons.lock_fill, 370),
+                ];
 
-        for (int i = 0; i < guardians.length; i++) {
-            profileItemDataList.add(
-                ProfileItemData(
-                    'Guardian ${i + 1}', 
-                    guardians[i].displayName, 
-                    CupertinoIcons.lock_fill, 
-                    370
-                )
-            );
-        }
-
-        return DefaultLayout(
-            title: 'My Profile',
-            content: [
-                Expanded(
-                    flex: 2,
-                    child: ProfileHeader(user: user)
-                ),
-                Expanded(
-                    flex: 8,
-                    child: Padding(
-                        padding: EdgeInsets.only(bottom: 10), 
-                        child: ProfileDetailsList(items: profileItemDataList)
+        if (!_isLoading) {for (int i = 0; i < guardians!.length; i++) {
+                profileItemDataList.add(
+                    ProfileItemData(
+                        'Guardian ${i + 1}', 
+                        guardians![i].displayName, 
+                        CupertinoIcons.lock_fill, 
+                        370
                     )
+                );
+            }}
+
+        return Stack(
+            children: [
+                DefaultLayout(
+                    title: 'My Profile',
+                    content: [
+                        Expanded(
+                            flex: 2,
+                            child: ProfileHeader(user: user!),
+                        ),
+                        Expanded(
+                            flex: 8,
+                            child: Padding(
+                                padding: EdgeInsets.only(bottom: 10),
+                                child: ProfileDetailsList(items: profileItemDataList),
+                            ),
+                        ),
+                    ],
                 ),
+                if (_isLoading) LoadingOverlay(),
+
             ],
         );
     }
