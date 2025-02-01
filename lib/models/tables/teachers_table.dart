1f4f2508a57dfb50f5373eb250ddaca26db7b70f
georgesycabaccang <georgesycabaccang@gmail.com>
2025-02-01 22:14:50 +0800

    Update teachers_table.dart and teacher.dart.

--- lib/models/tables/teachers_table.dart
+++ lib/models/tables/teachers_table.dart
@@ -1,7 +1,8 @@
 import 'package:powersync/powersync.dart';
 
 const teachersTable = Table('teachers', [
-  Column.text('user_id'),
-  Column.text('employed_date'),
-  Column.text('end_date'),
-]);
+        Column.text('user_id'),
+        Column.text('employed_date'),
+        Column.text('end_date'),
+        Column.text('teacher_no')
+    ]);
