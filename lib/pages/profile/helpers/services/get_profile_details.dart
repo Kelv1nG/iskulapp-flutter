cf47677ca08bf2b527c9b2f09f12fe268b3609e1
georgesycabaccang <georgesycabaccang@gmail.com>
2025-02-03 13:35:02 +0800

    Extract 'requests' for profile details from profile_page.dart into GetProfileDetails, and add null checks for studentItemDataList and teacherItemDataList.

---
+++ lib/pages/profile/helpers/services/get_profile_details.dart
@@ -0,0 +1,27 @@
+import 'package:school_erp/models/guardian.dart';
+import 'package:school_erp/models/student.dart';
+import 'package:school_erp/models/teacher.dart';
+import 'package:school_erp/repositories/guardian_repository.dart';
+import 'package:school_erp/repositories/student_repository.dart';
+import 'package:school_erp/repositories/teacher_repository.dart';
+
+class GetProfileDetails {
+    static Future<Map<String, dynamic>> forStudent(userId, academicYearId) async {
+        StudentRepository studentRepository = StudentRepository();
+        GuardianRepository guardianRepository = GuardianRepository();
+
+        List<Guardian> guardiansOfStudent = await guardianRepository.getGuardiansOfStudent(userId: userId);
+        Student studentDetails = await studentRepository.getStudent(userId: userId, academicYearId: academicYearId);
+
+        return {
+            "studentDetails": studentDetails,
+            "guardiansOfStudent": guardiansOfStudent
+        };
+    }
+
+    static Future<Teacher> forTeacher(userId) async {
+        TeacherRepository teacherRepository = TeacherRepository();
+        Teacher teacherDetails = await teacherRepository.getTeacher(userId: userId);
+        return teacherDetails;
+    }
+}
